c46cb5b510906cfc390c2c515b1948da5d14c439
Dzianis_Shyla <Dzianis_Shyla@epam.com>
2018-01-20 17:38:16 +0300

    two more tutorial steps completed

--- EntityTutorial/Controllers/HomeController.cs
+++ EntityTutorial/Controllers/HomeController.cs
@@ -1,23 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using EntityTutorial.DAL;
+using EntityTutorial.ViewModels;
 
 namespace EntityTutorial.Controllers
 {
     public class HomeController : Controller
     {
+        private SchoolContext db = new SchoolContext();
+
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult About()
+        public async Task<ActionResult> About()
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
+            IQueryable<EnrollmentDateGroup> data = from student in db.Students
+                group student by student.EnrollmentDate into dateGroup
+                select new EnrollmentDateGroup()
+                {
+                    EnrollmentDate = dateGroup.Key,
+                    StudentCount = dateGroup.Count()
+                };
+            
+            return View(await data.ToListAsync());
         }
 
         public ActionResult Contact()
@@ -25,6 +37,12 @@ namespace EntityTutorial.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
         }
     }
 }
